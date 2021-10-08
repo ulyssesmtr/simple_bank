0f22d0a3bf05b0878662e22c997c8db21eb1b82b
ulyssesmtr <ulyssesdmnt@gmail.com>
2021-10-08 18:15:23 -0300

    Change a few of the http methods used

--- bank_http.py
+++ bank_http.py
@@ -14,9 +14,13 @@ def load_base():
         acc.deposit(account[1])  # Update the account balance with the value present on the db
 
 
+with app.app_context():
+    load_base()
+
+
 @app.route("/")
 def home():
-    load_base()
+
     return render_template('front_page.html')
 
 
@@ -26,20 +30,20 @@ def create_account():
     account = bank1.create_account(last_number)
     db.insert_account(mysql, account.number, account.balance)
 
-    return f'The accccount number: {account.number} with a balance of {account.balance} was created'
+    return f'The acccount number: {account.number} with a balance of {account.balance} was created'
 
 
-@app.route("/account/balance", methods=['POST'])
+@app.route("/account/balance", methods=['GET'])
 def show_balance():
     try:
-        account_number = request.form['account_number']
+        account_number = request.args.get('account_number')
         account_balance = bank1.show_balance(int(account_number))
         return f'The account {account_number} has a balance of {account_balance}'
     except ValueError:
         return f'Invalid account'
 
 
-@app.route("/search/show_balance", methods=['POST'])
+@app.route("/search/show_balance", methods=['GET'])
 def search_show_balance():
 
     return render_template('search_show_balance.html')
@@ -58,7 +62,7 @@ def deposit():
         return 'Invalid account number or value'
 
 
-@app.route("/search/deposit", methods=['POST'])
+@app.route("/search/deposit", methods=['GET'])
 def search_deposit():
 
     return render_template('search_deposit.html')
@@ -77,8 +81,9 @@ def withdraw():
         return 'Invalid account number or value'
 
 
-@app.route("/search/withdraw", methods=['POST'])
+@app.route("/search/withdraw", methods=['GET'])
 def search_withdraw():
+
     return render_template('search_withdraw.html')
 
 
@@ -89,23 +94,17 @@ def transfer():
         account_number2 = int(request.form['account_number2'])
         value = int(request.form['value'])
         new_balance1, new_balance2 = bank1.transfer(account_number1, account_number2, value)
-        db.update_balance_db(new_balance1, account_number1)
-        db.update_balance_db(new_balance2, account_number2)
+        db.update_balance_db(mysql, new_balance1, account_number1)
+        db.update_balance_db(mysql, new_balance2, account_number2)
         return f'The value of {value} was transfered from the account {account_number1} to the account {account_number2}'
     except ValueError:
         return 'Invalid account number or value'
 
 
-@app.route("/search/transfer", methods=['POST'])
+@app.route("/search/transfer", methods=['GET'])
 def search_transfer():
 
     return render_template('search_transfer.html')
-
-
-@app.route("/search/delete", methods=['POST'])
-def search_delete():
-
-    return render_template('search_delete.html')
 
 
 @app.route("/account/delete", methods=['POST'])
@@ -117,6 +116,12 @@ def delete_account():
         return f'The account {account_number} was deleted'
     except ValueError:
         return 'Invalid account number'
+
+
+@app.route("/search/delete", methods=['GET'])
+def search_delete():
+
+    return render_template('search_delete.html')
 
 
 @app.route("/all_accounts", methods=['GET'])
