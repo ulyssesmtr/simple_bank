e2c03df01a1d86c597b2799179543fe9562e68a5
ulyssesmtr <ulyssesdmnt@gmail.com>
2021-10-08 07:52:53 -0300

    Refactoring

--- bank_http.py
+++ bank_http.py
@@ -1,67 +1,42 @@
 from flask import Flask, request, render_template
 import bank
-from flask_mysqldb import MySQL
-from db import select_accounts, setup
+import db
 
 app = Flask(__name__)
 bank1 = bank.Bank()
-
-# app.config['MYSQL_HOST'] = 'localhost'
-# app.config['MYSQL_USER'] = 'root'
-# app.config['MYSQL_PASSWORD'] = 'ulysses123'
-# app.config['MYSQL_DB'] = 'bank_accounts'
-# mysql = MySQL(app)
-
-mysql = setup(app)
+mysql = db.setup(app)
 
 
-def update_balance(new_balance, account_number):
-    cur = mysql.connection.cursor()
-    cur.execute("UPDATE account_list SET balance = (%s) WHERE number = (%s)", [new_balance, account_number])
-    mysql.connection.commit()
-    cur.close()
+def load_base():
+    account_list = db.select_accounts(mysql)
+    for account in account_list:
+        acc = bank1.create_account(account[0])  # Create the account using the number on the db
+        acc.deposit(account[1])  # Update the account balance with the value present on the db
 
 
 @app.route("/")
 def home():
-    # cur = mysql.connection.cursor()
-    # result = cur.execute("SELECT * from account_list")
-    # if result > 0:
-    #     account_list = cur.fetchall()
-    account_list = select_accounts(mysql)
-    for account in account_list:
-        acc = bank1.create_account(account[0])  # Create the account using the number on the db
-        acc.deposit(account[1])  # Update the account balance with the value present on the db
-    # cur.close()
+    load_base()
     return render_template('front_page.html')
 
 
 @app.route("/account", methods=['POST'])
 def create_account():
-    cur = mysql.connection.cursor()
-    result = cur.execute("SELECT * from account_list")
-    if result > 0:
-        account_list = cur.fetchall()
-        last_number = account_list[-1][0]
-        last_number += 1
-    else:
-        last_number = 5000
-
+    last_number = db.search_last_number(mysql)
     account = bank1.create_account(last_number)
-    account_number = account.number
-    account_balance = account.balance
-    cur.execute("INSERT INTO account_list (number, balance) VALUES(%s, %s)", [int(account_number), int(account_balance)])
-    mysql.connection.commit()
-    cur.close()
-    return f'The acccount number: {account.number} with a balance of {account.balance} was created'
+    db.insert_account(mysql, account.number, account.balance)
+
+    return f'The accccount number: {account.number} with a balance of {account.balance} was created'
 
 
 @app.route("/account/balance", methods=['POST'])
 def show_balance():
-    account_number = request.form['account_number']
-    account_balance = bank1.show_balance(int(account_number))
-    return f'The account {account_number} has a balance of {account_balance}'
-    # return bank1.show_balance(int(account_number))
+    try:
+        account_number = request.form['account_number']
+        account_balance = bank1.show_balance(int(account_number))
+        return f'The account {account_number} has a balance of {account_balance}'
+    except ValueError:
+        return f'Invalid account'
 
 
 @app.route("/search/show_balance", methods=['POST'])
@@ -72,12 +47,15 @@ def search_show_balance():
 
 @app.route("/account/deposit", methods=['POST'])
 def deposit():
-    account_number = int(request.form['account_number'])
-    value = int(request.form['value'])
-    new_balance = bank1.make_deposit(account_number, value)
-    update_balance(new_balance, account_number)
+    try:
+        account_number = int(request.form['account_number'])
+        value = int(request.form['value'])
+        new_balance = bank1.make_deposit(account_number, value)
+        db.update_balance_db(mysql, new_balance, account_number)
 
-    return f'The value of {value} was deposited to the account {account_number}'
+        return f'The value of {value} was deposited to the account {account_number}'
+    except ValueError:
+        return 'Invalid account number or value'
 
 
 @app.route("/search/deposit", methods=['POST'])
@@ -88,12 +66,15 @@ def search_deposit():
 
 @app.route("/account/withdraw", methods=['POST'])
 def withdraw():
-    account_number = int(request.form['account_number'])
-    value = int(request.form['value'])
-    new_balance = bank1.make_withdraw(account_number, value)
-    update_balance(new_balance, account_number)
+    try:
+        account_number = int(request.form['account_number'])
+        value = int(request.form['value'])
+        new_balance = bank1.make_withdraw(account_number, value)
+        db.update_balance_db(new_balance, account_number)
 
-    return f'The value of {value} was withdrawn from the account number {account_number}'
+        return f'The value of {value} was withdrawn from the account number {account_number}'
+    except ValueError:
+        return 'Invalid account number or value'
 
 
 @app.route("/search/withdraw", methods=['POST'])
@@ -103,13 +84,16 @@ def search_withdraw():
 
 @app.route("/account/transfer", methods=['POST'])
 def transfer():
-    account_number1 = int(request.form['account_number1'])
-    account_number2 = int(request.form['account_number2'])
-    value = int(request.form['value'])
-    new_balance1, new_balance2 = bank1.transfer(account_number1, account_number2, value)
-    update_balance(new_balance1, account_number1)
-    update_balance(new_balance2, account_number2)
-    return f'The value of {value} was transfered from the account {account_number1} to the account {account_number2}'
+    try:
+        account_number1 = int(request.form['account_number1'])
+        account_number2 = int(request.form['account_number2'])
+        value = int(request.form['value'])
+        new_balance1, new_balance2 = bank1.transfer(account_number1, account_number2, value)
+        db.update_balance_db(new_balance1, account_number1)
+        db.update_balance_db(new_balance2, account_number2)
+        return f'The value of {value} was transfered from the account {account_number1} to the account {account_number2}'
+    except ValueError:
+        return 'Invalid account number or value'
 
 
 @app.route("/search/transfer", methods=['POST'])
@@ -126,21 +110,18 @@ def search_delete():
 
 @app.route("/account/delete", methods=['POST'])
 def delete_account():
-    account_number = int(request.form['account_number'])
-    cur = mysql.connection.cursor()
-    cur.execute("DELETE FROM account_list WHERE number=%s", [account_number])
-    mysql.connection.commit()
-    cur.close()
-    bank1.delete_account(account_number)
-    return f'The account {account_number} was deleted'
+    try:
+        account_number = int(request.form['account_number'])
+        db.delete_account_db(mysql, account_number)
+        bank1.delete_account(account_number)
+        return f'The account {account_number} was deleted'
+    except ValueError:
+        return 'Invalid account number'
 
 
 @app.route("/all_accounts", methods=['POST'])
 def show_all_accounts():
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * from account_list")
-    result = cur.fetchall()
-    cur.close()
+    result = db.select_accounts(mysql)
     return render_template('accounts.html', result=result)
 
 
--- db.py
+++ db.py
@@ -1,5 +1,4 @@
 from flask_mysqldb import MySQL
-from bank_http import app
 
 
 def setup(app):
@@ -20,3 +19,38 @@ def select_accounts(mysql):
         return account_list
     else:
         return []
+
+
+def search_last_number(mysql):
+    cur = mysql.connection.cursor()
+    result = cur.execute("SELECT * from account_list")
+    if result > 0:
+        account_list = cur.fetchall()
+        last_number = account_list[-1][0]
+        last_number += 1
+        cur.close()
+        return last_number
+    else:
+        cur.close()
+        return 5000
+
+
+def insert_account(mysql, account_number, account_balance):
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO account_list (number, balance) VALUES(%s, %s)", [int(account_number), int(account_balance)])
+    mysql.connection.commit()
+    cur.close()
+
+
+def update_balance_db(mysql, new_balance, account_number):
+    cur = mysql.connection.cursor()
+    cur.execute("UPDATE account_list SET balance = (%s) WHERE number = (%s)", [new_balance, account_number])
+    mysql.connection.commit()
+    cur.close()
+
+
+def delete_account_db(mysql, account_number):
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM account_list WHERE number=%s", [account_number])
+    mysql.connection.commit()
+    cur.close()
